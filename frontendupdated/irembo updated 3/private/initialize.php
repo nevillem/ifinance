46c6f379a061c732d057710da75755d4fc5c071c
Blackson256 <nevillemwije@gmail.com>
2023-07-08 00:20:38 +0300

    Delete frontendupdated directory

--- frontendupdated/irembo updated 3/private/initialize.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once('Functions/General-Functions.php');
-$appName = 'iRembo Finance';
-$date1 = date('F j, Y', time());
-$date = date('F j, Y, g:i a', time());
-$year = date('Y');
-$month = date('F');
